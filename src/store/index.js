4d3341c511c9f8739c12b21edb4326f6dbb10f21
Yangwook <wooky92@naver.com>
2019-04-30 10:06:21 +0900

    Modify store index file

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { rootReducer } from 'store/reducers';
 import rootSaga from 'store/sagas';
@@ -7,12 +7,12 @@ import logger from 'redux-logger';
 const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware();
   const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__();
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const enhancers = composeEnhancers(applyMiddleware(sagaMiddleware, logger));
   const store = createStore(rootReducer, enhancers);
 
   rootSaga.map(sagaMiddleware.run);
+
   return store;
 };
 
