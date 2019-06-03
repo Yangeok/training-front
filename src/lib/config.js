cd3626f3dd073478af3476833258fffc1970ec21
Yangwook <wooky92@naver.com>
2019-06-03 21:55:03 +0900

    -

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,5 @@
 export default {
-  apiURL: 'http://52.79.70.237:3003/',
-  // apiURL: 'https://training-log-back.herokuapp.com/',
+  // apiURL: 'http://52.79.70.237:3003/',
+  apiURL: 'https://training-log-back.herokuapp.com/',
   header: { headers: { 'Access-Control-Allow-Origin': '*' } }
 };
